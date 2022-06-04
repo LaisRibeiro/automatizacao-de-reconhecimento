84598ed120fa062823fb65ca33d46df3d775d76d
LaisRibeiro <riibeirolais@gmail.com>
2022-06-03 21:08:38 -0300

    📝 Add case script

--- recon.sh
+++ recon.sh
@@ -8,17 +8,43 @@
 dominio=$1
 
 rm subdominio
-assetfinder $dominio | tee -a subdominio
-subfinder -silent -d $dominio 2>/dev/null | tee -a subdominio
-findomain-linux -t $dominio -q | tee -a subdominio
-clear
-echo "Filtrando Subdominios"
-cat subdominio | uniq -u > subdominios_filtrados
-clear
-echo "Resolvendo subdominios"
-clear
-echo "Subdominios 200 OK"
-cat subdominios_filtrados | httpx -silent -mc 200 | tee -a subdominios_resolvidos
-gowitness file -f subdominios_resolvidos
 
-done
+echo -ne "Acompanhar andamento do script via terminal? (S/N)"
+read -n1 -s escolha
+
+case $escolha in
+
+    "S" || "s")
+		assetfinder $dominio | -a subdominio
+        subfinder -silent -d $dominio 2>/dev/null | -a subdominio
+        findomain-linux -t $dominio -q | -a subdominio
+        clear
+        echo "Filtrando Subdominios"
+        cat subdominio | uniq -u > subdominios_filtrados
+        clear
+        echo "Resolvendo subdominios"
+        clear
+        echo "Subdominios 200 OK"
+        cat subdominios_filtrados | httpx -silent -mc 200 | -a subdominios_resolvidos
+        gowitness file -f subdominios_resolvidos
+
+        done
+	;;
+
+    "N" || "n")
+		assetfinder $dominio | tee -a subdominio
+        subfinder -silent -d $dominio 2>/dev/null | tee -a subdominio
+        findomain-linux -t $dominio -q | tee -a subdominio
+        clear
+        echo "Filtrando Subdominios"
+        cat subdominio | uniq -u > subdominios_filtrados
+        clear
+        echo "Resolvendo subdominios"
+        clear
+        echo "Subdominios 200 OK"
+        cat subdominios_filtrados | httpx -silent -mc 200 | tee -a subdominios_resolvidos
+        gowitness file -f subdominios_resolvidos
+
+        done
+	;;
+
