0ed425af8e8c90604313b71037e2ec97947eeefd
LaisRibeiro <riibeirolais@gmail.com>
2022-06-16 15:14:29 -0300

    ♻️ Edit consoles

--- recon.sh
+++ recon.sh
@@ -15,6 +15,7 @@ read -n1 -s escolha
 case $escolha in
 
     "S" || "s")
+    echo "Iniciando processo"
 		assetfinder $dominio | -a subdominio
         subfinder -silent -d $dominio 2>/dev/null | -a subdominio
         findomain-linux -t $dominio -q | -a subdominio
@@ -24,7 +25,7 @@ case $escolha in
         clear
         echo "Resolvendo subdominios"
         clear
-        echo "Subdominios 200 OK"
+        echo "Subdominios 200 OK, verifique no arquivo subdominios_filtrados"
         cat subdominios_filtrados | httpx -silent -mc 200 | -a subdominios_resolvidos
         gowitness file -f subdominios_resolvidos
 
@@ -32,6 +33,7 @@ case $escolha in
 	;;
 
     "N" || "n")
+        echo "Iniciando processo"
 		assetfinder $dominio | tee -a subdominio
         subfinder -silent -d $dominio 2>/dev/null | tee -a subdominio
         findomain-linux -t $dominio -q | tee -a subdominio
@@ -41,7 +43,7 @@ case $escolha in
         clear
         echo "Resolvendo subdominios"
         clear
-        echo "Subdominios 200 OK"
+        echo "Subdominios 200 OK, verifique no arquivo subdominios_filtrados"
         cat subdominios_filtrados | httpx -silent -mc 200 | tee -a subdominios_resolvidos
         gowitness file -f subdominios_resolvidos
 
